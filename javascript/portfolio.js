df1d0ba61d2ae5bd0e0b3c5a22a38240b9ab9dcd
afrin823 <afrin12490@gmail.com>
2024-03-14 14:37:57 +0600

    json file

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,3 +1,35 @@
+// ---------- portfolio project start ------------
+function loadProject(){
+    fetch('../Data/project.json')
+    .then(res => res.json())
+    .then(data => displayProject(data))
+}
+loadProject();
+
+function displayProject(data) {
+    const allProject = document.getElementById('project')
+
+    for(const protfolio of data){
+        const div = document.createElement('div');
+        div.innerHTML = `
+    
+        
+          <div class="item">
+          <img src="${protfolio.images}" class="img-fluid mb-4" alt="">
+          <span><i class="fa-solid fa-plus"></i></span>
+        </div>
+   
+    
+    
+
+  
+        `;
+        allProject.appendChild(div);
+    }
+}
+// ---------- portfolio project End ------------
+//===============================================
+// ---------- contact from start -----------------
 const form = document.querySelector('form');
 const firstName = document.querySelector('#fistName');
 const emailAddress = document.querySelector('#exampleInputEmail1');
@@ -24,3 +56,4 @@ function formHandler(e){
     subject.value = '',
     message.value = ''
 }
+// ---------- contact from end ------------
